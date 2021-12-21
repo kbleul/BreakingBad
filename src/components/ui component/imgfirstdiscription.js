8f6d8a53b635cfd8ff355e26ba9ff7c61d9ef155
kiBROm <danu@localhost.localdomain>
2021-12-21 10:20:05 -0500

    occupations section fix

--- src/components/ui component/imgfirstdiscription.js
+++ src/components/ui component/imgfirstdiscription.js
@@ -1,19 +1,37 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
 import { useState , useEffect } from 'react'
 import axios from 'axios'
 
 const Imgfirstdiscription = () => {
 
     const [characters,set_charachters] = useState({});
+    let occupations = [];
        
     useEffect(() => {
         const fetchitems = async () => {
             const result = await axios.get("https://www.breakingbadapi.com/api/characters?name=Walter+White");
                 set_charachters(result.data[0])
+                
+               ReactDOM.render(<RenderResult data={result.data[0].occupation}/>, document.getElementById("occupation"));
+        console.log(result.data[0].occupation)
+
+
         }
 
         fetchitems();
     },[])
+
+    const RenderResult = ({data}) => {
+            return(<>
+                {
+                    data.map(item => (
+                        <p key={item}  className="dispcription_para">{item}</p>
+                    ))
+                }
+                </>)
+    }
+
 
     return (
         <section className="dispcription_section">
@@ -22,7 +40,10 @@ const Imgfirstdiscription = () => {
         <p className="dispcription_para">{characters.appearance}</p>
         <p className="dispcription_para">{characters.nickname}</p>
         <p className="dispcription_para">{characters.portrayed}</p>
+        <ul id="occupation">
+        </ul>
         
+       
        
        
     </section>
